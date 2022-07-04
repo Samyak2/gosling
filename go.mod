256b8b9d77192d66b4c7fcdfdbeae83c906651d5
Samyak S Sarnayak <samyak201@gmail.com>
2022-07-04 16:58:28 +0530

    feat: initial working version

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/Samyak2/tts
+
+go 1.17
+
+require (
+	cloud.google.com/go/compute v1.3.0 // indirect
+	cloud.google.com/go/texttospeech v1.3.0 // indirect
+	github.com/alecthomas/kong v0.6.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.70.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+// Command quickstart generates an audio file with the content "Hello, World!".
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	texttospeech "cloud.google.com/go/texttospeech/apiv1"
+	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
+
+	"github.com/alecthomas/kong"
+)
+
+const (
+	languageCode = "en-US"
+	voiceName    = "en-US-Wavenet-F"
+)
+
+var cli struct {
+	Debug bool `help:"Enable debug mode."`
+
+	InputFile string `help:"Text file to read from. Use - for standard input." arg:""`
+
+	OutputFile string `help:"Audio file to write to. Use - for standard output." arg:""`
+}
+
+func runTTS(text string) ([]byte, error) {
+	// Instantiates a client.
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: languageCode,
+			Name:         voiceName,
+			// SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		return nil, err
+	}
+
+	// The resp's AudioContent is binary.
+	return resp.AudioContent, nil
+}
+
+func readFile(filename string) (string, error) {
+	if filename == "-" {
+		data, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return "", err
+		}
+		return string(data), err
+	}
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(data), err
+}
+
+func writeOutput(filename string, data []byte) error {
+	if filename == "-" {
+		_, err := os.Stdout.Write(data)
+		return err
+	}
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func main() {
+	kctx := kong.Parse(&cli)
+
+	input, err := readFile(cli.InputFile)
+	kctx.FatalIfErrorf(err)
+
+	audio, err := runTTS(input)
+	kctx.FatalIfErrorf(err)
+
+	err = writeOutput(cli.OutputFile, audio)
+	kctx.FatalIfErrorf(err)
+}
